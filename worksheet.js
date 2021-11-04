e00b9f77dc9989408e61bc775fa182572ceb28b9
cFadness <codie1989@gmail.com>
2021-11-04 15:29:45 -0700

    Added function for problem 1. Still debugging it.

--- worksheet.js
+++ worksheet.js
@@ -1,2 +1,39 @@
 "use strict"
 
+function isHappyNumber(positiveInteger){
+    let numString = positiveInteger.toString()
+    let numArray = []
+    let sqrNumArray
+    let sum
+    let x = true
+    let numNumArray
+    let i
+    while(x){
+        i=0
+        for(i=0; i<numString.length; i++){
+            numArray.push(numString[i])
+        }
+        numNumArray = numArray.map(function(element){
+           return parseInt(element)
+        })
+        sqrNumArray = numNumArray.map(function(element){
+            return element*element
+        })
+        sum = sqrNumArray.reduce(function(total, element){
+            return total + element
+        })
+        if(sum == 1){
+            x = false
+            return true
+        }
+        else if(sum == positiveInteger){
+            x = false
+            return false
+        }
+        else{
+            numString = sum.toString()
+        }
+    }
+}
+
+console.log(isHappyNumber(19))
